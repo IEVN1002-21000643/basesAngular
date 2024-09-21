471770df5989cbee82c216da5137263b3c89ef0d
Javier Esaú Castillo Villalobos <21000643@alumnos.utleon.edu.mx>
2024-09-20 20:52:42 -0600

    Commit viernes 20

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  //*ngIf=true o false validar si la lista tiene objetos
   productos:any[]=[
     {
       "productoID": 1,
@@ -16,7 +17,7 @@ export class ProductListComponent {
       "Year": 2022,
       "Marca": "NISSAN",
       "Color": "Azul",
-      "ImagenUrl": ""
+      "ImagenUrl": "https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_0c7166c16ff74073b65851f7f0b360ed.jpg"
     },
     {
       "productoID": 2,
@@ -26,7 +27,7 @@ export class ProductListComponent {
       "Year": 2022,
       "Marca": "AUDI",
       "Color": "Blanco",
-      "ImagenUrl": ""
+      "ImagenUrl": "https://acroadtrip.blob.core.windows.net/publicaciones-imagenes/Large/audi/a4/mx/RT_PU_4f813f5a5f4142d89f2244758c587ca0.webp"
     },
     {
       "productoID": 3,
@@ -36,7 +37,7 @@ export class ProductListComponent {
       "Year": 2020,
       "Marca": "KIA",
       "Color": "Azul",
-      "ImagenUrl": ""
+      "ImagenUrl": "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMzE2MzM5L0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNzE1NjMwMTcxNjQwLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjU0MCwiaGVpZ2h0IjozMTB9fX0="
     }
   ]
 }
